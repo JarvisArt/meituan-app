8d074a744ebc88cd0e8dbcbab9cea732748a98aa
Jarvis <1120886013@qq.com>
2019-04-27 23:04:19 +0800

    feat: 首页添加main内容

---
+++ components/index/artistic.vue
@@ -0,0 +1,114 @@
+<template>
+  <section class="m-istyle">
+    <dl @mouseover="over">
+      <dt>有格调</dt>
+      <dd
+        :class="{active:kind==='all'}"
+        kind="all"
+        keyword="酒店">全部</dd>
+      <dd
+        :class="{active:kind==='part'}"
+        kind="part"
+        keyword="美食">约会聚餐</dd>
+      <dd
+        :class="{active:kind==='movie'}"
+        kind="movie"
+        keyword="电影">电影演出</dd>
+    </dl>
+    <ul class="ibody">
+      <li
+        v-for="(item, index) in cur"
+        :key="index">
+        <el-card
+          :body-style="{ padding: '0px' }"
+          shadow="never">
+          <img
+            :src="item.img"
+            class="image">
+          <ul class="cbody">
+            <li class="title">{{ item.title }}</li>
+            <li class="pos"><span>{{ item.pos }}</span></li>
+            <li class="price">￥<em>{{ item.price }}</em><span>/起</span></li>
+          </ul>
+        </el-card>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data: () => {
+    return {
+      kind: 'all',
+      list: {
+        all: [],
+        part: [],
+        movie: [],
+      }
+    }
+  },
+  computed: {
+    cur: function () {
+      return this.list[this.kind]
+    }
+  },
+  async mounted(){
+    // let self=this;
+    // let {status,data:{count,pois}}=await self.$axios.get('/search/resultsByKeywords',{
+    //   params: {
+    //     keyword:'酒店',
+    //     city:self.$store.state.geo.position.city
+    //   }
+    // })
+    // if(status === 200 && count > 0){
+    //   let result = pois.map(item=>{
+    //     return {
+    //       title: item.name,
+    //       pos: item.pos,
+    //       price: item.price || '暂无',
+    //       img: item.photoUrl || 'https://p1.meituan.net/msmerchant/9480fc7bed1f8b92f69b7f8fde219da6273275.jpg@368w_208h_1e_1c',
+    //       url: item.url || 'https://qd.meituan.com/'
+    //     }
+    //   })
+    //   self.list[self.kind] = result
+    // }else{
+    //   self.list[self.kind] = []
+    // }
+  },
+  methods: {
+    // over: async function (e) {
+    //   let dom = e.target
+    //   let tag = dom.tagName.toLowerCase()
+    //   let self = this
+    //   if (tag === 'dd') {
+    //     this.kind = dom.getAttribute('kind')
+    //     let keyword = dom.getAttribute('keyword')
+    //     let {status,data:{count,pois}}=await self.$axios.get('/search/resultsByKeywords',{
+    //       params:{
+    //         keyword,
+    //         city:self.$store.state.geo.position.city
+    //       }
+    //     })
+    //     if(status === 200 && count > 0){
+    //       let result = pois.map(item=>{
+    //         return {
+    //           title: item.name,
+    //           pos: item.pos,
+    //           price: item.price || '暂无',
+    //           img: item.photoUrl || 'https://p1.meituan.net/msmerchant/9480fc7bed1f8b92f69b7f8fde219da6273275.jpg@368w_208h_1e_1c',
+    //           url: item.url || 'https://qd.meituan.com/'
+    //         }
+    //       })
+    //       self.list[self.kind] = result
+    //     }else{
+    //       self.list[self.kind] = []
+    //     }
+    //   }
+    // }
+  },
+
+}
+</script>
+<style lang="scss">
+    @import "@/assets/css/index/artistic.scss";
+</style>
--- components/index/menu.vue
+++ components/index/menu.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="e-menu-wrap">
-    <dl class="m-menu">
+  <div class="m-menu">
+    <dl class="nav" @mouseleave="mouseleave">
       <dt>全部分类</dt>
-      <dd v-for="(item, idx) in menu" :key="idx">
-        <i class="item.type"/>{{ item.title }}<span class="arrow"/>
+      <dd v-for="(item, idx) in menu" :key="idx" @mouseenter="mouseenter">
+        <i :class="item.type"/>{{ item.title }}<span sclass="arrow"/>
       </dd>
     </dl>
+    <div class="detail" v-if="kind" @mouseenter="sover" @mouseleave="sout">
+      <template v-for="(item, idx) in curdetail.child">
+        <h4 :key="idx">{{ item.title }}</h4>
+        <span v-for="v in item.child" :key="v">
+          {{ v }}
+        </span>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -13,20 +21,60 @@
 export default {
   data() {
     return {
+      kind: '',
       menu: [
         {
           type: 'food',
-          title: '美食'
+          title: '美食',
+          child: [
+            {
+              title: '美食',
+              child: ['代金券', '甜点饮品', '火锅', '自助餐', '小吃快餐']
+            }
+          ]
         },
         {
           type: 'takeout',
-          title: '外卖'
+          title: '外卖',
+          child: [
+            {
+              title: '外卖',
+              child: ['美团外卖']
+            }
+          ]
         },
         {
           type: 'hotel',
-          title: '酒店'
+          title: '酒店',
+          child: [
+            {
+              title: '酒店星级',
+              child: ['经济型', '舒适/三星', '高档/四星', '豪华/五星']
+            }
+          ]
         }
       ]
+    }
+  },
+  computed: {
+    curdetail() {
+      return this.menu.filter((item) => item.type === this.kind)[0];
+    }
+  },
+  methods: {
+    mouseleave() {
+      this._timer = setTimeout(() => {
+        this.kind = ''
+      }, 150)
+    },
+    mouseenter(e) {
+      this.kind = e.target.querySelector('i').className
+    },
+    sover() {
+      clearTimeout(this._timer)
+    },
+    sout() {
+      this.kind = ''
     }
   }
 }
