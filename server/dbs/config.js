f0cb09e4a5c6c89803240349bb023110fd949988
Jarvis <1120886013@qq.com>
2019-05-10 23:51:26 +0800

    feat: 注册发送验证码对接

--- server/dbs/config.js
+++ server/dbs/config.js
@@ -6,26 +6,26 @@ export default {
     },
     get prot() {
       return 6379
+    }
+  },
+  smtp: {
+    get host() {
+      return 'smtp.qq.com'
     },
-    smtp: {
-      get host() {
-        return 'smtp.qq.com'
-      },
-      get user() {
-        return '1120886013@qq.com'
-      },
-      get pass() {
-        return 'eqkadbravasdgeid'
-      },
-      get code() {
-        return () => {
-          return Math.random().toString(16).slice(2, 6).toUpperCase()
-        }
-      },
-      get expire() {
-        return () => {
-          return new Date().getTime() + 60 * 60 * 1000
-        }
+    get user() {
+      return '1120886013@qq.com'
+    },
+    get pass() {
+      return 'eqkadbravasdgeid'
+    },
+    get code() {
+      return () => {
+        return Math.random().toString(16).slice(2, 6).toUpperCase()
+      }
+    },
+    get expire() {
+      return () => {
+        return new Date().getTime() + 60 * 60 * 1000
       }
     }
   }
